9e39e07f75c46ea6bfc6d6b61a1966650301945b
Taisiia Simakova <stipaxa@gmail.com>
2024-07-03 17:09:06 +0000

    Move rules page responsive from JS to CSS media

--- assets/js/pages/rules-page.js
+++ assets/js/pages/rules-page.js
@@ -5,26 +5,6 @@ export function onRulesPageLoad() {
   // const levelForm = document.getElementById("game-level-form");
   // levelForm.addEventListener("submit", onClickStartGame);
 
-  // Change the view for screen widths larger than 576 pixels
-  if (document.body.clientWidth > 576) {
-    // Show elements with "rules-img" selector
-    const rulesImgs = document.querySelectorAll(".rules-img");
-    rulesImgs.forEach((rulesImg) => rulesImg.classList.remove("hidden"));
-
-    // Hide vertical text elements with "vertical-text" selector
-    const verticalTexts = document.querySelectorAll(".vertical-text");
-    verticalTexts.forEach((verticalText) =>
-      verticalText.classList.add("hidden")
-    );
-
-    // Hide fontawesome icon elements with "rules-icons-sm" class selector
-    const rulesIconsSmall = document.querySelectorAll(".rules-icons-sm");
-    console.log(rulesIconsSmall);
-    rulesIconsSmall.forEach((verticalText) =>
-      verticalText.classList.add("hidden")
-    );
-  }
-
   SetNewGameLevel("game-level-form");
   setUserIcon();
 }
